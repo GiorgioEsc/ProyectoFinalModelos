a65fd657cec9b4a565814c20843caabb9f78eef2
GiorgioEsc <32443837+GiorgioEsc@users.noreply.github.com>
2019-02-27 19:30:27 -0500

    Add files via upload

---
+++ Juego1/README.md
@@ -0,0 +1,53 @@
+# Juego Modelos
+
+## The kangaroo Runner (El Juego)
+
+* En este juego podremos vivir las increibles aventuras de Kangu quien trata de sobrevivir a los obstaculos que se le presentan mientras corre por su vida de una terrible y sangrienta jauria de hienas.
+
+#### Personajes
+
+En este increible juego tenemos la increible participacion de los personajes mas amados por los niños, los cuales son:
+
+##### Kangu (Como el Canguro atletico)
+![Kangu](https://raw.githubusercontent.com/GiorgioEsc/JuegoModelos/master/img/canguro.png)
+
+##### Rocky (Como el obstaculo y rival de Kangu)
+![Kangu](https://raw.githubusercontent.com/GiorgioEsc/JuegoModelos/master/img/cactus.png)
+
+##### Paisaje (Como el paisaje que nos recuerda que todo puede estar bien)
+![Kangu](https://raw.githubusercontent.com/GiorgioEsc/JuegoModelos/master/img/suelo.png)
+
+##### Nubes (Como las nubes que nos dan tranquilidad)
+![Kangu](https://raw.githubusercontent.com/GiorgioEsc/JuegoModelos/master/img/nube.png)
+
+##### Y la Increible participacion de Michifu (Como el gato que en cualquier momento quiere comer a Kangu)
+![Kangu](https://raw.githubusercontent.com/GiorgioEsc/JuegoModelos/master/img/fondo.jpg)
+
+
+### ¿Como se juega?
+
+Esta magnifica aventura se podra jugar presionando la barra espaciadora del teclado, asi podras saltar y evitar perder contra Rocky
+
+### ¿Como se gana?
+
+Esto es lo mejor del juego, se podra ganar cuando logres hacer como puntaje la edad del universo. ¡Perfecto! ¿no lo crees?
+
+### Manejo del nombre y marcador:
+
+El nombre se guarda en la variable homonima "nombre", declarada en la linea 1 del código javascript.<br/>
+La variable que lleva la cuenta del marcador se encuentra dentro del objeto "nivel", el cual está declarado en la linea 8 del código javascript. 
+Para acceder a la variable marcador del objeto nivel use la sentencia "nivel.marcador".
+
+##### ----------------------------------------------------------------------------------------------------------------------------------
+### Presentado por:
+##### Universidad Distrital Francisco Jose de Caldas.
+##### Facultad de Ingenieria.
+
+Jorge Antonio Escobar Bohorquez- 20152020120
+
+Cesar Alonso Llano Lagos- 20141020027
+
+##### Docente: 
+
+Alejandro Paolo Daza Corredor.
+
---
+++ Juego1/juego.js
@@ -0,0 +1,187 @@
+var nombre;
+var imgKanguro, imgNube, imgPiedras, imgSuelo;
+var ancho = 700;
+var alto = 300;
+var canvas,ctx;
+var suelo=220;
+var kanguro = {y:220, vy:0, gravedad:2, salto:28, vymax:9, saltando: false };
+var nivel= {velocidad: 9, marcador: 0, muerto:false};
+var piedras= {x: ancho+100, y:suelo};
+var nube= {x:400, y: 100,velocidad:1};
+var nube2= {x:200, y: 100,velocidad:1};
+
+document.addEventListener("keydown",function(evento){
+    if(evento.keyCode==32){
+        console.log("Salta");
+        if (nivel.muerto==false){
+            if(!kanguro.saltando){
+            saltar();
+            }
+         }
+        else{
+            nivel.velocidad=9;
+            nube.velocidad=1;
+            nube2.velocidad=1;
+            
+            piedras.x= ancho+100;
+            nube.x = ancho+100;
+            nube2.x= ancho+300;
+            nivel.marcador=0;
+            nivel.muerto=false;
+
+        }
+    }
+});
+
+function cargaImagenes(){
+    imgKanguro= new Image ();
+    imgNube= new Image ();
+    imgNube2= new Image ();
+    imgPiedras= new Image ();
+    imgSuelo= new Image ();
+
+    imgKanguro.src = 'img/canguro.png'
+    imgNube.src = 'img/nube.png';
+    imgNube2.src = 'img/nube2.png';
+    imgPiedras.src = 'img/cactus.png'
+    imgSuelo.src = 'img/suelo.png'
+}
+
+function inicializa(){
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+    ingresarNombre("Ingrese su nombre");
+    cargaImagenes();
+}
+
+function ingresarNombre(txt){
+    let aux = prompt(txt, "nombre");
+
+    if(aux == null || aux == ""){
+        ingresarNombre("nombre invalido, introduzca otro.")
+    }
+    else{
+        nombre = aux;
+    } 
+}
+
+function borraCanvas(){
+    canvas.width=ancho;
+    canvas.height=alto;
+}
+
+function dibujaKanguro(){
+    ctx.drawImage(imgKanguro,0,0,485,485,100,kanguro.y,80,80)
+}
+
+function dibujaPiedras(){
+    ctx.drawImage(imgPiedras,0,0,517,517,piedras.x, piedras.y,38,75);
+}
+
+function logicaPiedras(){
+    if (piedras.x <- 100){
+        piedras.x = ancho+100;
+    }
+    else{
+        piedras.x -= nivel.velocidad;
+    }
+}
+
+function dibujaSuelo(){
+    ctx.drawImage(imgSuelo,0,0,960,480,0, 30, 690,300 );
+}
+
+function dibujaNube(){
+    ctx.drawImage(imgNube,0,0,485,485,nube.x, nube.y, 82,31 );
+}
+
+function logicaNube(){
+    if (nube.x <- 100){
+        nube.x = ancho+100;
+    }
+    else{
+        nube.x -= nube.velocidad;
+    }
+}
+
+function dibujaNube2(){
+    ctx.drawImage(imgNube2,0,0,517,517,nube2.x, nube2.y, 82,31 );
+}
+function logicaNube2(){
+    if (nube2.x <- 100){
+        nube2.x = ancho+100;
+    }
+    else{
+        nube2.x -= nube2.velocidad;
+    }
+}
+
+function saltar(){
+    kanguro.saltando=true;
+    kanguro.vy = kanguro.salto;
+}
+
+function gravedad(){
+    if(kanguro.saltando==true){
+        if (kanguro.y-kanguro.vy-kanguro.gravedad > suelo){
+            kanguro.saltando=false;
+            kanguro.vy=0;
+            kanguro.y=suelo;
+        }
+        else{
+            kanguro.vy -= kanguro.gravedad;
+            kanguro.y -= kanguro.vy;
+        }
+    }
+}
+
+function colision(){
+    if (piedras.x >=100 && piedras.x<=180){
+        if(kanguro.y >= suelo-55){
+            nivel.muerto= true;
+            nivel.velocidad=0;
+            nube.velocidad=0;
+            nube2.velocidad=0;
+
+        }
+    }
+}
+
+function puntuacion(){
+    ctx.font = "30px impact";
+    ctx.fillStyle = '#000000';
+    ctx.fillText(nivel.marcador, 600, 50);
+
+    if(nivel.muerto==true){
+        ctx.font="60px impact";
+        ctx.fillStyle = ('red');
+        ctx.fillText('GAME OVER', 240,150);
+    }
+    else{
+        nivel.marcador++;
+    }
+}
+
+
+//Bucle Principal 
+var FPS = 60;
+setInterval(function(){ //cada cuanto se debe ejecutar una funcion en un intervalo de tiempo.
+    principal();
+}, 1000/FPS);
+
+
+//Esta es la funcion donde el juego va actualizar todo.
+function principal (){
+    borraCanvas();
+    gravedad();
+    colision();
+    logicaPiedras();
+    logicaNube();
+    logicaNube2();
+    dibujaSuelo();
+    dibujaPiedras();
+    dibujaNube();
+    dibujaNube2();
+    dibujaKanguro();
+    puntuacion();
+}
